74fc3209c1928b21242485e6410afc12ce376027
hasaki99 <lihua3413@gmail.com>
2023-03-28 11:24:52 +0800

    Site updated: 2023-03-28 11:24:50

--- js/matery.js
+++ js/matery.js
@@ -105,6 +105,10 @@ $(function () {
             // 启用字幕
             subHtmlSelectorRelative: true
         });
+        // 懒加载防止插件冲突
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+            });
 
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
